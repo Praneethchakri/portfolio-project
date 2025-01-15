ef94e699ba8f6f8884a4f3657e8239c913295bba
DELL <praneeth.chakri26@gmail.com>
2025-01-16 00:26:27 +0530

    Portofolio Changes

--- portfolio-webSite/src/app/app.component.ts
+++ portfolio-webSite/src/app/app.component.ts
@@ -11,4 +11,13 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio-webSite';
+
+
+  onDownloadResume(): void {
+    const resumeUrl = 'assets/PRANEETH CHAKRAVARTHI DIGUVAPALE Sr. Java.docx'; // Path to your resume file in the assets folder
+    const link = document.createElement('a'); // Create a temporary <a> element
+    link.href = resumeUrl;
+    link.download = 'PRANEETH CHAKRAVARTHI DIGUVAPALE Sr. Java.docx';  // Set the downloaded file name
+    link.click(); // Programmatically click the link to trigger the download
+  }
 }
--- portfolio-webSite/src/app/app.routes.ts
+++ portfolio-webSite/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import {ProjectListComponent} from './components/project-list/project-list.component';
 
 // Define the standalone routes
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // Default route
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path:'projects',component:ProjectListComponent}
 ];
 export class AppRoutingModule {}
